a6124681f2d1504bb7b8e9ff7120e91cba030d24
misha <mishall.afzal@gmail.com>
2018-01-05 09:35:04 -0800

    added radio buttons

--- react-client/src/components/RecipeList.jsx
+++ react-client/src/components/RecipeList.jsx
@@ -6,7 +6,29 @@ const RecipeList = (props) => (
 	<div>
 	<div className="results">
 	<h3>Recipes</h3>
+	<form>
+		<div className="radio">
+          <label>
+            <input type="radio" value="option1"  />
+            Most Popular
+          </label>
+        </div>
+        <div className="radio">
+          <label>
+            <input type="radio" value="option2" />
+            Uses All Ingredients
+          </label>
+        </div>
+        <div className="radio">
+          <label>
+            <input type="radio" value="option3" />
+            Uses Additional Ingredients
+          </label>
+        </div>
+	</form>
+	<div>
 	{ props.recipes.map(recipe => <RecipeItem key={recipe.id} recipe={recipe} onSave={props.onSave}/>)}
+	</div>
 	</div>
 
 
